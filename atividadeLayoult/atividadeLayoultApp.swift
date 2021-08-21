a5ce6f2aea3919a94eca8beeffbf038bad659e7b
Rafael Lara <rafaelsilva.l1995@gmail.com>
2021-08-21 15:14:48 -0300

    Initial Commit

---
+++ atividadeLayoult/atividadeLayoultApp.swift
@@ -0,0 +1,17 @@
+//
+//  atividadeLayoultApp.swift
+//  atividadeLayoult
+//
+//  Created by Rafael Silva on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct atividadeLayoultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
